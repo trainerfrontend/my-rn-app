b55efa199dfdc6630c6b92b6dc361ba3709f3be0
trainerfrontend <praveen.trainer.@outlook.com>
2023-10-04 11:30:25 +0100

    feat: TS conversion

--- app/posts.tsx
+++ app/posts.tsx
@@ -1,16 +1,22 @@
 import { Text, StyleSheet, View, FlatList } from 'react-native';
 import { useEffect, useState } from 'react';
 import ListItem from '../components/ListItem';
+import { User } from '../interfaces/user';
 
+// prop -> URL ->string
 
-export default function page() {
+interface iPostPage {
+    URL?: string
+}
 
-    const [users, setUsers] = useState([]);
+const Page: React.FC<iPostPage> = () => {
+
+    const [users, setUsers] = useState<User[]>([]);
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
-            .then((data) => setUsers(data))
+            .then((data: User[]) => setUsers(data))
     }, [])
 
 
@@ -19,7 +25,7 @@ export default function page() {
         <Text style={styles.heading}>users</Text>
         <FlatList
             data={users}
-            keyExtractor={item => item.id}
+            keyExtractor={item => item.id + ""}
             renderItem={({ item }) => <ListItem listItem={item} />
             } />
 
---
+++ interfaces/user.ts
@@ -0,0 +1,21 @@
+export interface User {
+    id: number
+    name: string
+    username: string
+    email: string
+    address: Address
+  }
+  
+  export interface Address {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: Geo
+  }
+  
+  export interface Geo {
+    lat: string
+    lng: string
+  }
+  
